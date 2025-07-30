2da4e31c4fa1e89294be27a1f34d7a4018449e22
Kartik <kartik07mane@gmail.com>
2025-07-30 15:41:53 +0530

    final changes

--- backend/server.js
+++ backend/server.js
@@ -38,3 +38,4 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
